79a241d1524f2a5819269b1133dda85507b7e1ee
zcchen <Bill.Chen.fs.cn@outlook.com>
2025-05-13 00:13:55 +0800

    Support variable assigning. e.g.: 'a=balabala, b=$a'

--- lua/dotenv.lua
+++ lua/dotenv.lua
@@ -1,7 +1,5 @@
 ---@diagnostic disable: missing-parameter
 -- main module file
-local uv = vim.loop
-
 local dotenv = {}
 
 dotenv.config = {
@@ -12,14 +10,12 @@ dotenv.config = {
 }
 
 local function notify(msg, level)
-  if not dotenv.config.verbose then
+  if (not dotenv.config.verbose) and (level ~= "ERROR" or level ~= "WARN") then
     return
   end
-
   if level == nil then
     level = "INFO"
   end
-
   vim.notify(msg, vim.log.levels[level])
 end
 
@@ -31,19 +27,53 @@ local function read_file(path)
   return data
 end
 
+local function get_shell_varname(s)
+  local bracket_l, varname, bracket_r = string.match(s, '^%$(%{*)([_%a]+[_%w]*)(%}*)$')
+  local msg = string.format("bracket_l: '%s', bracket_r: '%s', varname: '%s'", bracket_l, bracket_r, varname)
+  notify(msg)
+  if ((bracket_l == "" and bracket_r == "") or (bracket_l == "{" and bracket_r == "}"))
+      and varname ~= "" then -- bracket matched, continue...
+    return varname
+  else
+    msg = string.format("var '%s' has incorrect format, must be like ${VarName} or $Var_Name.", s)
+    notify(msg, "ERROR")
+  end
+end
+
 local function parse_data(data)
   local out = {}
-  local notify_msg = ""
+  local msg = ""
   for line in string.gmatch(data, "[^\r\n]+") do
     local l = vim.trim(line)
-    notify_msg = string.format(">>> raw line: <%s>", l)
-    notify(notify_msg)
+    msg = string.format(">>> raw line: '%s'", l)
+    notify(msg)
     if not vim.startswith(l, "#") and l ~= "" then
       -- filter-out the comment line nor empty line
       local k, v = string.match(l, '^([%w_]+)%s*=%s*(.*)$')
-      notify_msg = string.format(">>> found key: <%s>, value: <%s>", k, v)
-      notify(notify_msg)
-      out[k] = v
+      msg = string.format(">>> found key: '%s', value: '%s'", k, v)
+      notify(msg)
+      if v == "" then                      -- set empty string value
+        out[k] = ""
+      elseif not string.find(v, '%$') then -- common value data, set it normally
+        out[k] = v
+      else                                 -- try to find whether <v> is a defined varname.
+        local varname = get_shell_varname(v)
+        if not varname then                -- varname is invalid, return {}
+          return {}
+        end
+        local is_varname_defined = false
+        for kk, vv in pairs(out) do
+          if varname == kk then -- found the defined varname
+            out[k] = vv
+            is_varname_defined = true
+            break
+          end
+        end
+        if not is_varname_defined then
+          msg = string.format("var '%s' is NOT defined before, skip to modify env '%s'", v, k)
+          notify(msg, "WARN")
+        end
+      end
     end
   end
   return out
@@ -58,21 +88,24 @@ local function get_env_file()
 end
 
 local function load(file)
+  local msg = ""
   if file == nil then
     file = get_env_file()
   end
-
   local ok, data = pcall(read_file, file)
   if not ok then
-    notify(".env file not found", "ERROR")
+    msg = string.format("file '%s' not found.", file)
+    notify(msg, "ERROR")
     return
   end
-
   local env = parse_data(data)
   for k, v in pairs(env) do
+    msg = string.format("setenv '%s' to '%s'", k, v)
+    notify(msg)
     vim.fn.setenv(k, v)
   end
-  notify(".env file loaded")
+  msg = string.format("file '%s' loaded.", file)
+  notify(msg)
 end
 
 dotenv.setup = function(args)
