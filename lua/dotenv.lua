b003d53a06a8fbe269f936fcd0a6dce3b7e92e82
zcchen <Bill.Chen.fs.cn@outlook.com>
2025-05-13 17:22:41 +0800

    Expose the functions dotenv.load() & dotenv.get(), and reconstruct the dotenv.setup() function.

--- lua/dotenv.lua
+++ lua/dotenv.lua
@@ -87,11 +87,9 @@ local function get_env_file()
   return files[1]
 end
 
-local function load(file)
+
+dotenv.load = function(file)
   local msg = ""
-  if file == nil then
-    file = get_env_file()
-  end
   local ok, data = pcall(read_file, file)
   if not ok then
     msg = string.format("file '%s' not found.", file)
@@ -108,45 +106,43 @@ local function load(file)
   notify(msg)
 end
 
+dotenv.get = function(varname)
+  print(vim.env[varname])
+end
+
 dotenv.setup = function(args)
   dotenv.config = vim.tbl_extend("force", dotenv.config, args or {})
-
-  vim.api.nvim_create_user_command("Dotenv", function(opts)
-    dotenv.command(opts)
-  end, { nargs = "?", complete = "file" })
-  vim.api.nvim_create_user_command("DotenvGet", function(opts)
-    dotenv.get(opts.fargs)
-  end, { nargs = 1 })
-
+  vim.api.nvim_create_user_command("Dotenv",
+    function(opts)
+      local filename
+      if opts ~= nil and #opts.fargs > 0 then
+        filename = opts.fargs[1]
+      else
+        filename = get_env_file()
+      end
+      dotenv.load(filename)
+    end,
+    { nargs = "?", complete = "file" }
+  )
+  vim.api.nvim_create_user_command("DotenvGet",
+    function(opts)
+      dotenv.get(opts.fargs)
+    end,
+    { nargs = 1 }
+  )
   if dotenv.config.enable_on_load then
     local group = vim.api.nvim_create_augroup("Dotenv", { clear = true })
-    vim.api.nvim_create_autocmd(dotenv.config.event, { group = group, pattern = "*", callback = dotenv.autocmd })
+    vim.api.nvim_create_autocmd(
+      dotenv.config.event, {
+        pattern = "*",
+        group = group,
+        callback = function()
+          local f = get_env_file()
+          dotenv.load(f)
+        end,
+      }
+    )
   end
-end
-
-dotenv.get = function(arg)
-  local var = string.upper(arg[1])
-  if vim.env[var] == nil then
-    print(var .. ": not found")
-    return
-  end
-  print(vim.env[var])
-end
-
-dotenv.autocmd = function()
-  load()
-end
-
-dotenv.command = function(opts)
-  local args
-
-  if opts ~= nil then
-    if #opts.fargs > 0 then
-      args = opts.fargs[1]
-    end
-  end
-
-  load(args)
 end
 
 return dotenv
